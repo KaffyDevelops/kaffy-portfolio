b398e3932b5b84fd1109f86a89d4a62b1691606c
Kafayat Faniran <64654420+KaffyDevelops@users.noreply.github.com>
2026-09-03 16:00:13 +0100

    feat: assemble optimised professional portrait asset

---
+++ src/lib/portrait.ts
@@ -0,0 +1,6 @@
+import part1 from "./portrait-parts/part1";
+import part2 from "./portrait-parts/part2";
+import part3 from "./portrait-parts/part3";
+import part4 from "./portrait-parts/part4";
+
+export const kaffyPortrait = `data:image/webp;base64,${part1}${part2}${part3}${part4}`;
